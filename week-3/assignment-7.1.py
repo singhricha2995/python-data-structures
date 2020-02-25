633ef92d14c1dc698af108fabe037c4c27be8eee
singhricha2995 <61435466+singhricha2995@users.noreply.github.com>
2020-02-25 11:05:40 -0500

    Update assignment-7.1.py

--- week-3/assignment-7.1.py
+++ week-3/assignment-7.1.py
@@ -1,3 +1,5 @@
+#Write a program that prompts for a file name, then opens that file and reads through the file, and print the contents of the file in upper case. Use the file words.txt to produce the output below.
+
 fname=input('Enter the file name:')
 fn=open(fname,'r')
 for lines in fn:
