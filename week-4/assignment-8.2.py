d4173e6e516b60d24117cf0933f8e316b8fa1cf6
singhricha2995 <61435466+singhricha2995@users.noreply.github.com>
2020-02-25 12:19:40 -0500

    Create assignment-8.2.py

---
+++ week-4/assignment-8.2.py
@@ -0,0 +1,14 @@
+#Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() method. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in alphabetical order.
+
+fname = input("Enter file name: ")
+fn=open(fname,'r')
+lst=list()
+for lines in fn:
+    words=lines.rstrip().split()
+    for letters in words:
+        if letters in lst:
+            continue
+        if letters not in lst:
+            lst.append(letters)
+lst.sort()
+print(lst)
